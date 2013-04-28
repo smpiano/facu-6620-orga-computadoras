437d5f15dc2ac3e7e30a91f9396ea8f3783dc003
Guido <guido321@gmail.com>
2013-04-28 15:39:22 -0300

    cambios en ambos metodos de ordenamiento ambos estan con la declaracion de la funcion correcta, como (char ** words, int arraysize)
    bubblesort funcionando completo, estaria "terminado"
    shellsort aun no funciona con las words

--- tp1/source/bubblesort.c
+++ tp1/source/bubblesort.c
@@ -2,22 +2,31 @@
 #include <string.h>
 
 // Print array
-void printArray(int numbers[], int array_size) {
+void printArrayInt(int numbers[], int arraysize) {
   printf("Array = [");
   int i = 0;
-  for (i = 0; i < array_size; i++) {
+  for (i = 0; i < arraysize; i++) {
     printf("%d, ",numbers[i]);
   }
   printf("]\n");
 }
 
+// Print Strings array
+void printStringsArray(char ** words, int arraysize) {
+  printf("Array = [");
+  int i = 0;
+  for (i = 0; i < arraysize; i++) {
+    printf("%s,", words[i]);
+  }
+  printf("]\n");
+}
+
 //Bubble sort algorithm
-void bubbleSort(int numbers[], int array_size) {
-  int i = 0; // bla
+void bubbleSortInt(int numbers[], int arraysize) {
+  int i = 0;
   int j = 0;
   int temp = 0;
- 
-  for (i = (array_size - 1); i > 0; i--) {
+  for (i = (arraysize - 1); i > 0; i--) {
     for (j = 1; j <= i; j++) {
       if (numbers[j-1] > numbers[j]) {
         temp = numbers[j-1];
@@ -28,10 +37,32 @@ void bubbleSort(int numbers[], int array_size) {
   }
 }
 
+void swap(char **p, char **q) {
+  char *tmp;
+  tmp = *p;
+  *p = *q;
+  *q = tmp;
+}
+
+void bubbleSort(char ** words, int arraysize) {
+ int i = 0;
+ int j = 0;
+
+ for(i = 0; i < arraysize; ++i)
+    for(j = i + 1; j < arraysize; ++j)
+      if(strcmp(words[i], words[j]) > 0)
+        swap(&words[i], &words[j]);
+}
+
 //main for bubblesort
 int main(int argc, char **argv) {
+  char *words[] = {"hola","chau","and","zero","void","HOLA","donde","kilo","kilometro","kil","kill"};
+  printStringsArray(words,11);
+  bubbleSort(words, 11);
+  printStringsArray(words,11);
+
   int nro[11] = {123,46,78,0,1,2,3,7,567,89,10};
-  printArray(nro, 11);
-  bubbleSort(nro, 11);
-  printArray(nro, 11);
+  printArrayInt(nro, 11);
+  bubbleSortInt(nro, 11);
+  printArrayInt(nro, 11);
 }
--- tp1/source/shellsort.c
+++ tp1/source/shellsort.c
@@ -2,24 +2,34 @@
 #include <string.h>
 
 // Print array
-void printArray(int numbers[], int array_size) {
+void printArrayInt(int numbers[], int arraysize) {
   printf("Array = [");
   int i = 0;
-  for (i = 0; i < array_size; i++) {
+  for (i = 0; i < arraysize; i++) {
     printf("%d, ",numbers[i]);
   }
   printf("]\n");
 }
 
+// Print Strings array
+void printStringsArray(char ** words, int arraysize) {
+  printf("Array = [");
+  int i = 0;
+  for (i = 0; i < arraysize; i++) {
+    printf("%s,", words[i]);
+  }
+  printf("]\n");
+}
+
 //Shell sort algorithm
-void shellSort(int numbers[], int array_size) {
-  int i = 0; 
+void shellSortInt(int numbers[], int arraysize) {
+  int i = 0;
   int j = 0;
   int m = 0;
   int mid = 0;
 
-  for (m = array_size/2; m > 0; m/=2) {
-    for (j = m; j< array_size; j++) {
+  for (m = arraysize/2; m > 0; m/=2) {
+    for (j = m; j< arraysize; j++) {
       for (i = j - m; i >= 0; i -= m) {
         if (numbers[i + m] >= numbers[i])
           break;
@@ -33,10 +43,28 @@ void shellSort(int numbers[], int array_size) {
   }
 }
 
+void swap(char **p, char **q) {
+  char *tmp;
+  tmp = *p;
+  *p = *q;
+  *q = tmp;
+}
+
+void shellSort(char ** words, int arraysize) {
+  // pendiente de hacer
+}
+
 //main for shellsort
 int main(int argc, char **argv) {
+
+  char *words[] = {"hola","chau","and","zero","void","HOLA","donde","kilo","kilometro","kil","kill"};
+  printStringsArray(words,11);
+  shellSort(words, 11);
+  printStringsArray(words,11);
+
   int nro[11] = {123,46,78,0,1,2,3,7,567,89,10};
-  printArray(nro, 11);
-  shellSort(nro, 11);
-  printArray(nro, 11);
+  printArrayInt(nro, 11);
+  shellSortInt(nro, 11);
+  printArrayInt(nro, 11);
+
 }
