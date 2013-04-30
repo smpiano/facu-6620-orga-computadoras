e2f48d7969ffd170af6fefca8d3ee7602d636acf
Sergio Piano <smpiano@gmail.com>
2013-04-30 17:17:27 -0300

    Agregando <= en los limites de las variables para que compare todos los elementos.

--- tp1/source/bubblesort.c
+++ tp1/source/bubblesort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "bubblesort.h"
 
 // Print array
@@ -49,8 +50,8 @@ void bubblesort(char** words, unsigned int arraysize) {
  int i = 0;
  int j = 0;
  printStringsArray(words, arraysize);
- for(i = 0; i < arraysize; ++i)
-    for(j = i + 1; j < arraysize; ++j)
+ for(i = 0; i <= arraysize; ++i)
+    for(j = i + 1; j <= arraysize; ++j)
       if(strcasecmp(words[i], words[j]) >= 0)
         swap(&words[i], &words[j]);
 }
